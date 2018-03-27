e0253d44bb504ddce57f4f766943d09979e86f3b
ihtram <m.ihtram@arbisoft.com>
2018-03-27 19:07:01 +0500

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gradebook-edx-platform-extensions',
-    version='1.0.6',
+    version='1.1.7',
     description='User grade management extension for edX platform',
     long_description=open('README.rst').read(),
     author='edX',
