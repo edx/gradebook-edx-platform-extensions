5a42ca001d4f1f6be774cfc57dd332b3fd447f93
attiyaIshaque <atiya.ishaq@arbisoft.com>
2020-05-15 16:44:10 +0500

    Fix warnings of course key field.

--- gradebook/migrations/0001_initial.py
+++ gradebook/migrations/0001_initial.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import model_utils.fields
-from openedx.core.djangoapps.xmodule_django.models import CourseKeyField
+from opaque_keys.edx.django.models import CourseKeyField
 import django.utils.timezone
 from django.conf import settings
 
